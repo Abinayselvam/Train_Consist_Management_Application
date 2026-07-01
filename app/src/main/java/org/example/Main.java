55ecdbde7c62bf8f80e9d17865677aa84f564d5c
AbinayaSelvam <sabinaya352@gmail.com>
2026-07-01 12:14:08 +0530

    Implement the Track Unique Bogie IDs

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -45,6 +47,22 @@ public class Main {
 
         //Display Initial Consist Information
         System.out.println("\nCurrent Train Consist Count: "+passengerBogies.size());
+
+        System.out.println("============================================");
+        System.out.println("=== UC3 Bogie with unique ID's ===");
+        System.out.println("============================================");
+
+        Set<String> bogiesId= new HashSet<>();
+        bogiesId.add("BG101");
+        bogiesId.add("BG102");
+        bogiesId.add("BG103");
+        bogiesId.add("BG104");
+        bogiesId.add("BG101");//Duplicate Id
+        bogiesId.add("BG102");//Duplicate Id
+
+        //
+        System.out.println("Hash set store only store the Unique ID's");
+        System.out.println("Bogies Id: "+bogiesId);
 
 
     }
