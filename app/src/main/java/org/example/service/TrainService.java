e2b2d2b69ecee95e81920f3dce35fd276ecddf04
AbinayaSelvam <sabinaya352@gmail.com>
2026-07-02 11:50:08 +0530

    Implemented the Filter Passenger Bogies Using Streams

--- app/src/main/java/org/example/service/TrainService.java
+++ app/src/main/java/org/example/service/TrainService.java
@@ -4,6 +4,7 @@ import org.example.model.Bogie;
 import org.example.model.Train;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class TrainService {
@@ -140,6 +141,36 @@ public class TrainService {
         System.out.println("-------------------------");
 
         for (Bogie bogie : bogies) {
+
+            System.out.println(
+                    bogie.getName()
+                            + " --> "
+                            + bogie.getCapacity());
+
+        }
+
+    }
+    public void filterPassengerBogies() {
+
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println();
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Second Sitting", 90));
+
+        List<Bogie> filteredBogies =
+                bogies.stream()
+                        .filter(bogie -> bogie.getCapacity() > 60)
+                        .collect(Collectors.toList());
+
+        System.out.println("Passenger Bogies with Capacity > 60");
+        System.out.println("-----------------------------------");
+
+        for (Bogie bogie : filteredBogies) {
 
             System.out.println(
                     bogie.getName()
